1ba11ef7402c2f5b1aa779e46b7df5ac82f04be9
suzakijun1 <dyong1997@gmail.com>
2023-04-21 11:57:42 -0400

    added svg2 tests

---
+++ svg2.test.js
@@ -0,0 +1,23 @@
+const SVG = require("./svg");
+
+test("should throw an error if text exceeds 3 characters", () => {
+  const expectedError = new Error("Text must not exceed 3 characters.");
+  const svg = newSVG();
+  expect(() => svg.setText("morethanthreeletters", "#fff")).toThrow(
+    expectedError
+  );
+});
+
+test("should set text message and color", () => {
+  const expectedSvg =
+    '<svg version ="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="#333">SVG</text></svg';
+  const svg = new SVG();
+  svg.setText("SVG", "#333");
+  expect(svg.render()).toEqual(expectedSvg);
+});
+
+test("should render a 300 x 200 svg element", () => {});
+
+test("should append text element", () => {});
+
+test("should include a shape", () => {});
