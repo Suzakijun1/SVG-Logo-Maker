3d24204db48c99c600c12712bea61f711739f8c3
suzakijun1 <dyong1997@gmail.com>
2023-04-21 12:12:09 -0400

    added shapes2

---
+++ shapes2.js
@@ -0,0 +1,13 @@
+class Shape {
+  // What do we need?
+}
+
+class Circle extends Shape {
+  render() {
+    //Returns a string that represents a circle element, using literals to pass color.
+  }
+}
+
+//Create a Triangle and Square class that extends the Shape class.
+
+module.exports = { Circle, Triangle, Square };
