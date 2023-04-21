f14a5826f8d455ade941d2922125bdcd62e24098
suzakijun1 <dyong1997@gmail.com>
2023-04-21 12:09:26 -0400

    added svg2.js info

---
+++ svg2.js
@@ -0,0 +1,16 @@
+class SVG {
+  constructor() {
+    this.textElement = "";
+    this.shapeElement = "";
+  }
+
+  // research how to create an SVG file!!
+
+  // Need to create a render method that returns a string that represents an SVG file with our passed in elements.
+
+  // We need to create a setText method that takes in a message and a color and sets the textElement property to a string that represents a text element, using literals to pass in the message and color.
+
+  // We need to create a setShape method that takes in a shape and sets the shapeElement property to the shape's render method.
+}
+
+module.exports = SVG;
