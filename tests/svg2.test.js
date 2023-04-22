0324ace4b8708b3c444f1078d1565c860337e955
suzakijun1 <dyong1997@gmail.com>
2023-04-22 15:26:10 -0400

    svg2 test changes

--- tests/svg2.test.js
+++ tests/svg2.test.js
@@ -1,4 +1,4 @@
-const SVG = require("./svg");
+const SVG = require("../lib/svg2");
 
 test("should throw an error if text exceeds 3 characters", () => {
   const expectedError = new Error("Text must not exceed 3 characters.");
@@ -9,14 +9,17 @@ test("should throw an error if text exceeds 3 characters", () => {
 });
 
 test("should set text message and color", () => {
-  const expectedSvg =
-    '<svg version ="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="#333">SVG</text></svg';
+  const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><text x="150" y="125" font-size="60" text-anchor="middle" fill="#333">SVG</text></svg>`;
   const svg = new SVG();
   svg.setText("SVG", "#333");
   expect(svg.render()).toEqual(expectedSvg);
 });
 
-test("should render a 300 x 200 svg element", () => {});
+test("should render a 300 x 200 svg element", () => {
+  const expectedSize = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>`;
+  const svg = new SVG();
+  expect(svg.render()).toEqual(expectedSize);
+});
 
 test("should append text element", () => {});
 
