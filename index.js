daef426b3938bcc21bedef08514213a72b51dbb8
suzakijun1 <dyong1997@gmail.com>
2023-04-22 15:25:17 -0400

    index changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const { Circle, Square, Triangle } = require("./lib/shapes2");
+const SVG = require("./lib/svg2");
 
 class CLI {
   run() {
@@ -39,6 +41,27 @@ class CLI {
         // create some conditional logic to generate a new instance of the shape class, and pass in user input!
         // Create a new instance of the SVG class and set the text and shapeColor.
         // return the writeFile method, passing in the file name and the rendered svg.
+        let shape;
+
+        switch (shapeType) {
+          case "circle":
+            shape = new Circle();
+            break;
+          case "square":
+            shape = new Square();
+            break;
+          case "triangle":
+            shape = new Triangle();
+            break;
+        }
+
+        shape.setColor(textColor);
+
+        const svg = new SVG();
+        svg.setText(text, textColor);
+        svg.setShape(shape);
+        return writeFile("newSvg.svg", svg.render());
+        // function
       })
       .then(() => {
         // log a message to the console that the file has been generated.
