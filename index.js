7720111b8b1e401ecb7faa5aacb77aa6c57eaeee
suzakijun1 <dyong1997@gmail.com>
2023-04-22 14:05:42 -0400

    more progress

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ class CLI {
     return inquirer
       .prompt([
         // Prompt the user for the specs they want to display in the SVGAElement.
+        {
+          type: "input",
+          name: "text",
+          message: "Enter up to three characters for your SVG",
+          validate: (nameInput) => {
+            if (nameInput.length > 3) {
+              return true;
+            } else {
+              console.log("You must up to three characters.");
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "textColor",
+          message: "What would you like the text color to be?",
+        },
+        {
+          type: "checkbox",
+          name: "shape",
+          message: "Please choose a shape",
+          choices: ["circle", "triangle", "square"],
+        },
+        {
+          type: "input",
+          name: "shapeColor",
+          message: "What would you like the shape color to be?",
+        },
       ])
       .then(({ text, textColor, shapeType, shapeColor }) => {
         // create some conditional logic to generate a new instance of the shape class, and pass in user input!
